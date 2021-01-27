ef1f1ac9e0a883e498507a4b010f4d2588abad34
andrawan <andrawan.ep@gmail.com>
2021-01-28 00:30:56 +0700

    font poppins for all page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar">
+    navbar
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import Search from '../views/Search.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
   }
 ]
 
