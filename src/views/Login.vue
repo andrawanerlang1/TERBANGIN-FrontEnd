1ddec10ecbe90100fc59141d5d06ca481f4abab6
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2021-01-28 00:21:19 +0700

    auth page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class="home"></div>
+  <div class="Login poppins">
+    <b-container fluid>
+      <b-row>
+        <b-col md="7" lg="7" xl="7" style="padding:0px">
+          <div class="img">
+            <img src="../assets/img/AuthImg.png" alt="" class="responsive" />
+          </div>
+        </b-col>
+        <b-col md="5" lg="5" xl="5" style="padding:0px">
+          <Register />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;900&family=Rubik:wght@300;400;500;600;700&display=swap');
+.poppins {
+  font-family: 'Poppins', sans-serif;
+}
+img {
+  width: 100%;
+}
+</style>
+
 <script>
+import Register from '../components/_base/Auth/Register'
 export default {
   name: 'Login',
-  components: {}
+  components: {
+    Register
+  }
 }
 </script>
