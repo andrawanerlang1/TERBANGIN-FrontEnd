c195658afc3b0b1e76d1fe03d8d504c2aa41b8e3
andrawan <andrawan.ep@gmail.com>
2021-01-28 02:16:36 +0700

    footer

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
     <Navbar />
+    <div class="mains">
+      isi search
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default {
   name: 'Search',
-  components: { Navbar }
+  components: { Navbar, Footer }
 }
 </script>
+
+<style scoped>
+.mains {
+  background-color: #dfe1e9;
+}
+</style>
