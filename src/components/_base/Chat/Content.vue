bbeb6ebc4bb252708d741319e3d13e0dcf73837a
rizqonmaulana <rizqonmaulana5@gmail.com>
2021-01-29 13:47:23 +0800

    add chat page

---
+++ src/components/_base/Chat/Content.vue
@@ -0,0 +1,211 @@
+<template>
+  <div>
+    <div class="chat-content">
+      <div class="header">
+        <div class="d-flex flex-row align-items-center">
+          <div class="profile-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="profile-name">
+            <p class="ml-3">Firman - Admin</p>
+          </div>
+        </div>
+      </div>
+      <div class="chat-list">
+        <div class="chat" v-for="item in pesanList" :key="item">
+          <div v-if="userId !== item.id" class="left mt-1">
+            <div class="d-flex flex-row align-items-end">
+              <div class="img mr-2">
+                <img
+                  src="../../../assets/img-admin.png"
+                  class="profile-img-chat "
+                />
+              </div>
+              <div class="msg">
+                <p>{{ item.pesan }}</p>
+              </div>
+            </div>
+          </div>
+          <div v-else class="right mt-1">
+            <div class="d-flex flex-row align-items-end">
+              <div class="msg ml-auto">
+                <p>
+                  {{ item.pesan }}
+                </p>
+              </div>
+              <div class="img ml-2">
+                <img
+                  src="../../../assets/img-admin.png"
+                  class="profile-img-chat "
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="chat-input">
+        <div class="input-border">
+          <input type="text" placeholder="type your message" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  data() {
+    return {
+      userId: 1,
+      pesanList: [
+        {
+          id: 1,
+          pesan: 'hallo teman'
+        },
+        {
+          id: 2,
+          pesan: 'uy hallo broh'
+        },
+        {
+          id: 2,
+          pesan: 'pesan dari kiri'
+        },
+        {
+          id: 1,
+          pesan: 'pesan kanan'
+        },
+        {
+          id: 2,
+          pesan: 'kiri lagi'
+        },
+        {
+          id: 1,
+          pesan: 'kanan dong'
+        },
+        {
+          id: 1,
+          pesan: 'kanan lagi nih'
+        },
+        {
+          id: 2,
+          pesan: 'uy hallo broh'
+        },
+        {
+          id: 2,
+          pesan: 'pesan dari kiri'
+        },
+        {
+          id: 1,
+          pesan: 'pesan kanan'
+        },
+        {
+          id: 2,
+          pesan: 'kiri lagi'
+        },
+        {
+          id: 1,
+          pesan: 'kanan dong'
+        },
+        {
+          id: 1,
+          pesan: 'kanan lagi nih'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  height: 70px;
+}
+.chat-list {
+  height: 500px;
+  border-right: 1px solid #ededed;
+  border-left: 1px solid #ededed;
+  padding: 10px;
+  overflow: auto;
+  background: #fff;
+}
+.chat-input {
+  height: 100px;
+}
+
+p {
+  margin-bottom: unset;
+}
+
+input {
+  border: unset;
+  border-radius: 20px;
+  padding: 10px;
+  width: 100%;
+  outline: unset;
+}
+
+.input-border {
+  background: #ededed;
+  padding: 10px;
+}
+
+.header {
+  background: #ededed;
+  padding: 10px;
+}
+
+.profile-name {
+  font-weight: 600;
+  font-size: 18px;
+}
+
+.profile-img img {
+  height: 45px;
+  width: 45px;
+  border-radius: 10px;
+}
+
+.profile-img-chat {
+  height: 35px;
+  width: 35px;
+  border-radius: 10px;
+}
+
+.left .msg {
+  background: rgba(0, 17, 255, 0.8);
+  padding: 8px 15px 8px 20px;
+  border-radius: 15px 10px 15px 5px;
+  color: white;
+  max-width: 50%;
+}
+
+.right {
+  margin-left: auto;
+}
+
+.right .msg {
+  background: rgba(221, 221, 221, 0.8);
+  padding: 8px 20px 8px 15px;
+  border-radius: 15px 15px 5px 15px;
+  color: rgb(0, 0, 0);
+  max-width: 50%;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #d1d1d1;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(177, 177, 177);
+}
+</style>
---
+++ src/components/_base/Chat/Room.vue
@@ -0,0 +1,345 @@
+<template>
+  <div>
+    <div class="room-container">
+      <div class="header">
+        <h4>Chat</h4>
+      </div>
+      <div class="room-list">
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-start py-2 room-item">
+          <div class="chat-img">
+            <img src="../../../assets/img-admin.png" />
+          </div>
+          <div class="chat-msg">
+            <div class="d-flex flex-column">
+              <div>
+                <p><strong>Firman - Admin</strong></p>
+              </div>
+              <div>
+                <p class="text-grey">Thanks for your help</p>
+              </div>
+            </div>
+          </div>
+          <div class="chat-info">
+            <div class="d-flex flex-column ">
+              <div class="text-right"><p class="text-grey time">08:30</p></div>
+
+              <div class="unread-msg rounded-circle text-center mt-1">
+                <p class="mt-1">14</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.room-container {
+  background: #fff;
+  padding: 10px;
+  height: 633px;
+}
+
+.room-list {
+  overflow: auto;
+  height: 560px;
+  padding-right: 8px;
+}
+
+p {
+  margin-bottom: unset;
+}
+
+img {
+  border-radius: 20px;
+  width: 50px;
+  height: 50px;
+}
+
+.chat-img {
+  width: 20%;
+}
+
+.chat-msg {
+  width: 65%;
+}
+
+.chat-info {
+  width: 15%;
+}
+
+.text-grey {
+  color: #6b6b6b;
+}
+
+.time {
+  font-size: 12px;
+}
+
+.unread-msg {
+  background: #2395ff;
+  color: #fff;
+  width: 23px;
+  height: 23px;
+  font-size: 12px;
+  margin-left: auto;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #d1d1d1;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(177, 177, 177);
+}
+</style>
